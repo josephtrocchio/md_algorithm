d73ea8dacd1d7a11c046fcbc602bb8c547eadbc7
llrantzll <trocchioje@gmail.com>
2021-10-15 20:39:08 -0400

    added imports and documentation to main

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import metrics
+
+
 def main():
+    #Import the data
+
+    #Converting categorical data to numbers
+
+    #Split into training test and test set
+
+    #Normalize/Scale the data
+
+    #Pick model and Train
+
     pass
 
 
