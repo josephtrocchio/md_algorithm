d20b6b93a9d9d429bb7cf83a3a5023bcd75984c9
llrantzll <trocchioje@gmail.com>
2021-10-25 13:41:32 -0400

    fixed the value attribute error. The model is working, but useless in its current state

--- main.py
+++ main.py
@@ -9,12 +9,21 @@ from sklearn import metrics
 def main():
     #Import the data
     data = pd.read_csv('gps_data.csv')
-    print(data.head())
-    print(data.info())
+
+
+    #Drop NaN values, as well as infinite values that bug the "float64's"
+    data.dropna(axis=0)
+    data = data.isin([np.inf, -np.inf])
+    print(data)
+
 
     #Dividing Data into features(x) and labels(y)
-    X = data
-    y = data.iloc[: , :5]
+    X = data.iloc[: , :5]
+    y = data.iloc[:, 5:]
+
+    # print(X.isnull().values.any())
+    # print(X.isnull().sum().sum())
+    # print(X.shape)
 
     #Converting categorical data to numbers
     numerical = X.drop(['Player Name', 'Period Name', 'Position Name'], axis=1)
@@ -22,6 +31,7 @@ def main():
 
     cat_numerical = pd.get_dummies(categorical, drop_first=True)
     X = pd.concat([numerical, cat_numerical], axis=1)
+
 
     #Split into training test and test set
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.20, random_state=0)
@@ -34,16 +44,16 @@ def main():
     
 
     #Pick model and Train
-    # rf_reg = RandomForestRegressor(random_state=42, n_estimators=500)
-    # regressor = rf_reg.fit(X_train, y_train)
-    # y_pred = regressor.predict(X_test)
+    rf_reg = RandomForestRegressor(random_state=42, n_estimators=500)
+    regressor = rf_reg.fit(X_train, y_train)
+    y_pred = regressor.predict(X_test)
+    print(y_pred)
+
 
     #Evaluate model with metrics module
-    # print('Mean Absolue Error:', metrics.mean_absolute_error(y_test, y_pred))
-    # print('Mean Squared Error:', metrics.mean_squared_error(y_test,  y_pred))
-    # print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
-
-    pass
+    print('Mean Absolue Error:', metrics.mean_absolute_error(y_test, y_pred))
+    print('Mean Squared Error:', metrics.mean_squared_error(y_test,  y_pred))
+    print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
 
 
 if __name__ == "__main__":
